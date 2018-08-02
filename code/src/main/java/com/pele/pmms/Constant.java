c0373e7d4d9a8d8544cd7ba292a1a62cd8d84883
pele <kldy@qq.com>
2018-08-02 17:42:07 +0800

    提交Code目录

---
+++ code/src/main/java/com/pele/pmms/Constant.java
@@ -0,0 +1,6 @@
+package com.pele.pmms;
+
+public interface Constant {
+
+	String TEST_C = "MMMMMMMMM";
+}
---
+++ code/src/main/java/com/pele/pmms/Test.java
@@ -0,0 +1,8 @@
+package com.pele.pmms;
+
+public class Test {
+
+	public static String test(String i){
+		return Constant.TEST_C + " - " + i;
+	}
+}
